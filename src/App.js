eeeb1005b53e626e91b89cd1d977945e70be8bf0
JuanMaBlanco <jmblanco686@gmail.com>
2021-09-02 11:41:20 -0300

    agregue el navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+
+
+
+const App = () => {
+
+   const nameStyle = {
+  backgroundColor: "black",
+  color: "white",
+ 
+};
+
+  return( 
+
+    <div>
+      <NavBar />
+      <h1 className="title">Bienvenido</h1>
+      <div>
+    <p style={nameStyle}>La Taberna Del Cervecero </p>
+    <p>las mejores cervezas del Pais</p>
+    </div>
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./NavBar.css"
+
+
+function NavBar() {
+  return (
+    <div className="navbar">
+      <div>thebirrashop</div>
+      <div>Inicio</div>
+      <div>cervezas</div>
+      <div>Compras</div>
+    </div>
+  )
+}
+
+export default NavBar;
